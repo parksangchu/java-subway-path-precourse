e0a82d305c4a230f99fdd79bde242cab75a607c4
sangchu <didi1484@gmail.com>
2023-12-11 19:14:34 +0900

    feat: 메인 메뉴 목록 관리 기능 추가

---
+++ src/main/java/subway/domain/MainMenu.java
@@ -0,0 +1,13 @@
+package subway.domain;
+
+public class MainMenu {
+    private final String name;
+
+    public MainMenu(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
